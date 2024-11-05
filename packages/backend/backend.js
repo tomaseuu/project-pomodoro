264ea8e29996c63a8a970751032f4484e8c0487e
Madison Lopez <madison.lopez.8404@gmail.com>
2024-11-05 10:29:05 -0800

    Added all User CRUD operation on backend

--- packages/backend/backend.js
+++ packages/backend/backend.js
@@ -1,6 +1,7 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import user_services from "./user_services.js";
 
 const app = express();
 const port = 8000;
@@ -8,12 +9,62 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
+const idGen = (length) => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let i = 0;
+  while (i < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    i += 1;
+  }
+  return result;
+}
 
 app.get("/", (req, res) => {
   res.send("Hi Everyone!");
 });
 
+app.get("/users", (req, res) => {
+  const username = req.query.username;
+  user_services.getUsers(username)
+  .then((result) => {
+    if (result) {
+      const users = {users: result}
+      res.send(users)
+    } else {
+      res.send([]);
+    }
+  })
+  .catch((error) => {
+    res.status(404).send("Users Not Found")
+  });
+});
 
+app.post("/users", (req, res) => { // 201
+  const userId = idGen(6);
+  const userToAdd = {id: userId, username: req.body.username, email: req.body.email, password: req.body.password};
+  user_services.addUser(userToAdd).then((result) => 
+    res.status(201).send(result));
+});
+
+app.patch('/users/:id', (req, res) => {
+  const id = req.params.id; 
+  const updatedUser = {id: id, username: req.body.username, email: req.body.email, password: req.body.password};
+  user_services.updateUserById(id, updatedUser).then((result) =>
+    res.status(200).send(result));
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id; 
+  console.log("Deleting user with ID:", id); // Debugging line
+  user_services.deleteUserById(id).then((result) => {
+      res.status(204).send(result);
+  })
+  .catch((error) => {
+    res.status(404).send("User Not Found")
+  });
+});
 
 app.listen(port, () => {
   console.log(
---
+++ packages/backend/user_services.js
@@ -0,0 +1,74 @@
+
+import mongoose from "mongoose";
+import userModel from "./user_schema.js";
+
+mongoose.set("debug", true);
+
+mongoose.connect("mongodb://localhost:27017/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).catch((error) => console.log(error));
+
+
+function getUsers(username, email) {
+  let promise;
+  if (username === undefined && email === undefined) {
+    promise = userModel.find();
+  } else if (username && !email) {
+    promise = findUserByUsername(username);
+  } else if (email && !username) {
+    promise = findUserByEmail(email);
+  }else if (email && username){
+    promise = findUserByUsernameAndEmail(username, email)
+  }  
+  return promise;
+}
+
+function findUserByUsername(username) {
+  return userModel.find({ username: username });
+}
+
+function findUserByEmail(email) {
+  return userModel.find({ email: email });
+}
+
+function findUserByUsernameAndEmail(username, email) {
+  return userModel.find({ username: username, email: email })
+}
+
+function findUserById(id) {
+  return userModel.findById(id);
+}
+
+function addUser(user) {
+  const userToAdd = new userModel(user);
+  const promise = userToAdd.save();
+  return promise;
+}
+
+function deleteUserById(id) {
+  return userModel.findByIdAndDelete(id)
+  .then((result) => {
+   console.log("User deleted:", result); // Log the result
+   return result;
+   }).catch((error) => {
+   console.log("Error deleting user:", error); // Log the error
+   throw error;
+ });
+}
+
+function updateUserById(id, updatedUser) {
+  return userModel.findByIdAndUpdate(id, updatedUser, {new: true});
+}
+
+
+export default {
+  addUser,
+  getUsers,
+  findUserById,
+  findUserByEmail,
+  findUserByUsername,
+  findUserByUsernameAndEmail,
+  deleteUserById,
+  updateUserById
+};
