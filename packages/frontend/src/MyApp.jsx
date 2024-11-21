133a471ccdb170549a4e5f3d318ff4a0a8fc4599
Thomas Le <lethomas998@gmail.com>
2024-11-21 11:26:19 -0800

    TA4 completed pt2

--- packages/frontend/src/MyApp.jsx
+++ packages/frontend/src/MyApp.jsx
@@ -1,22 +1,29 @@
 // src/MyApp.jsx
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Table from "./Table";
+import Form from "./Form";
+import Login from "./Login";
+
+const API_PREFIX = "http://localhost:8000";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+  const [token, setToken] = useState(null);
+  const [message, setMessage] = useState("");
 
   function removeOneCharacter(index) {
     const character = characters[index];
     const id = character._id;
 
-    fetch(`http://localhost:8000/users/${id}`, {
+    fetch(`${API_PREFIX}/users/${id}`, {
       method: "DELETE",
     })
       .then((response) => {
         if (response.status === 204) {
           const updated = characters.filter((_, i) => i !== index);
           setCharacters(updated);
-        } else if (response.status === 404) {
-          console.log("User not found. Couldn't delete.");
         } else {
           console.log("Error deleting the user.");
         }
@@ -36,9 +43,11 @@ function MyApp() {
         console.log(error);
       });
   }
+
   function fetchUsers() {
-    const promise = fetch("http://localhost:8000/users");
-    return promise;
+    return fetch(`${API_PREFIX}/users`, {
+      headers: addAuthHeader(),
+    });
   }
 
   function postUser(person) {
@@ -53,10 +62,56 @@ function MyApp() {
     return promise;
   }
 
+  function loginUser(creds) {
+    fetch(`${API_PREFIX}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(creds),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json().then((data) => {
+            setToken(data.token);
+            setMessage("Login successful!");
+          });
+        }
+        setMessage(`Login Error: ${res.status}`);
+      })
+      .catch((err) => setMessage(`Login Error: ${err}`));
+  }
+
+  function signupUser(creds) {
+    fetch(`${API_PREFIX}/signup`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(creds),
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          return res.json().then((data) => {
+            setToken(data.token);
+            setMessage("Signup successful!");
+          });
+        }
+        setMessage(`Signup Error: ${res.status}`);
+      })
+      .catch((err) => setMessage(`Signup Error: ${err}`));
+  }
+
+  function addAuthHeader(headers = {}) {
+    return token ? { ...headers, Authorization: `Bearer ${token}` } : headers;
+  }
+
   useEffect(() => {
     fetchUsers()
-      .then((res) => res.json())
-      .then((json) => setCharacters(json["users_list"]))
+      .then((res) => (res.status === 200 ? res.json() : undefined))
+      .then((json) => {
+        if (json) {
+          setCharacters(json["users_list"]);
+        } else {
+          setCharacters(null);
+        }
+      })
       .catch((error) => {
         console.log(error);
       });
@@ -64,9 +119,28 @@ function MyApp() {
 
   return (
     <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form handleSubmit={updateList} />
+      {message && <p>{message}</p>}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Table
+                characterData={characters}
+                removeCharacter={removeOneCharacter}
+              />
+              <Form handleSubmit={updateList} />
+            </>
+          }
+        />
+        <Route path="/login" element={<Login handleSubmit={loginUser} />} />
+        <Route
+          path="/signup"
+          element={<Login handleSubmit={signupUser} buttonLabel="Sign Up" />}
+        />
+      </Routes>
     </div>
   );
 }
+
 export default MyApp;
