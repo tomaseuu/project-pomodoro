55e44d733ac8572585122d11a742dfc88a2a754e
Thomas Le <lethomas998@gmail.com>
2024-12-03 15:34:49 -0800

    undo changes

--- packages/backend/task-schema.js
+++ packages/backend/task-schema.js
@@ -1,14 +1,14 @@
 //task_schema
 import mongoose from "mongoose";
-const mongoose = require("mongoose");
+
 
 const TaskSchema = new mongoose.Schema(
-  {
-    userId: { type: String, required: true },
-    task: { type: String, required: true },
-    completed: { type: Boolean, default: false },
-  },
-  { collection: "Tasks" }
+    {
+      userId: { type: String, required: true },
+      task: { type: String, required: true },
+      completed: { type: Boolean, default: false }
+    },
+    { collection: "Tasks" }
 );
 
 const Task = mongoose.model("Task", TaskSchema);
