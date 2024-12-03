e79290a7d4161802b0090d939f88803a27da912d
Thomas Le <lethomas998@gmail.com>
2024-12-03 15:59:10 -0800

    lets see

--- packages/backend/user_schema.test.js
+++ packages/backend/user_schema.test.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
-import User from "./user_schema";
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const User = require("./user_schema");
 
 jest.setTimeout(10000);
 
