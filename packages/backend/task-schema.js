c67630941a0bb06df27f2c2dd87b9a73a2f3f5f2
christinadechalk <cdechalk@calpoly.edu>
2024-12-04 11:36:56 -0800

    tast schema commit

--- packages/backend/task-schema.js
+++ packages/backend/task-schema.js
@@ -2,21 +2,12 @@
 import mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema(
-<<<<<<< HEAD
     {
       userId: { type: String, required: true },
       task: { type: String, required: true, trim: true },
       completed: { type: Boolean, default: false }
     },
     { collection: "Tasks" }
-=======
-  {
-    userId: { type: String, required: true },
-    task: { type: String, required: true },
-    completed: { type: Boolean, default: false },
-  },
-  { collection: "Tasks" }
->>>>>>> f684cdc89f2aafc7077ccca15598ea2d3451503e
 );
 
 const Task = mongoose.model("Task", TaskSchema);
