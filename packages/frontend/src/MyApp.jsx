e2df1f5c1f1cdffdab72bcc96892252642ff74c8
Thomas Le <lethomas998@gmail.com>
2024-12-04 10:30:12 -0800

    changed myapp and routes

--- packages/frontend/src/MyApp.jsx
+++ packages/frontend/src/MyApp.jsx
@@ -5,6 +5,8 @@ import { Routes, Route } from "react-router-dom";
 import Table from "./Table";
 import Form from "./Form";
 import Login from "./Login";
+import Contact from "./Contact";
+import Pomodoro from "./Pomodoro";
 
 const API_PREFIX = "http://localhost:8000";
 
@@ -138,6 +140,8 @@ function MyApp() {
           path="/signup"
           element={<Login handleSubmit={signupUser} buttonLabel="Sign Up" />}
         />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/pomodoro" element={<Pomodoro />} />
       </Routes>
     </div>
   );
