2629d84a014c20eb3d1b81a31f819c8f6baf630a
christinadechalk <cdechalk@calpoly.edu>
2024-12-03 21:50:48 -0800

    todo list fixed

---
+++ packages/frontend/main.jsx
@@ -0,0 +1,16 @@
+// src/main.jsx
+
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+
+function MyApp() {
+  return <div></div>;
+}
+// Create the container
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the Root
+root.render(<MyApp />);
